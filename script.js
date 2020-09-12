081a9122ed941961a3f923f7cf12161c286d8b56
Nikolai <nvanbaak@gmail.com>
2020-09-12 16:19:59 -0700

    start button now starts a timer

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Declare global variables
+var startPage = document.getElementById("startPage");
+var timeLimit = 75;
 
 // When the document loads, we unhide the start page
-
-var startPage = document.getElementById("startPage");
 document.body.onload = startPage.classList.toggle("hideMe");
 
-// Determines start button behavior
+// DStart button functionality
 document.getElementById("startBtn").addEventListener("click", function() {
 
     // Hides the start page
@@ -13,6 +14,26 @@ document.getElementById("startBtn").addEventListener("click", function() {
     // Makes timer visible
     document.getElementById("timer").classList.toggle("hideMe");
 
+    // Start the timer
+    var timer = setInterval(function() {
+
+        // While there's still time on the clock
+        if ( timeLimit > 0 ) {
+        
+            // Decrease the clock
+            timeLimit--;
+            console.log(timeLimit);
+
+        } else {
+
+            // Otherwise stop the clock and move to Game Over
+            clearInterval(timer);
+        }
+    }, 1000);
+    
 });
+
+
+
 
 
