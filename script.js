3d344847e3191549aa81f5e024d6e46af75101be
Nikolai <nvanbaak@gmail.com>
2020-09-12 21:26:06 -0700

    timer now outputs to the timer display instead of console

--- script.js
+++ script.js
@@ -20,9 +20,9 @@ document.getElementById("startBtn").addEventListener("click", function() {
         // While there's still time on the clock
         if ( timeLimit > 0 ) {
         
-            // Decrease the clock
+            // Decrease the clock and update the display
             timeLimit--;
-            console.log(timeLimit);
+            updateTimeDisplay(timeLimit);
 
         } else {
 
@@ -30,10 +30,27 @@ document.getElementById("startBtn").addEventListener("click", function() {
             clearInterval(timer);
         }
     }, 1000);
-    
+
 });
 
 
+function updateTimeDisplay(timeValue) {
+    // This function takes a time value in seconds and outputs it to the timer display
+    
+    // get the seconds value using modulo 60
+    var timeSeconds = timeValue % 60;
+    // get the minutes value by subtracting the seconds and dividing by 60
+    var timeMinutes = Math.floor( (timeValue - timeSeconds) / 60 )
+
+    // if the seconds value is under 10 we need to add another 0 or it looks weird
+    if ( timeSeconds < 10 ) {
+        timeSeconds = "0" + timeSeconds;
+    } 
+
+    // Output the values
+    document.getElementById("timerOutput").innerText = (timeMinutes + ":" + timeSeconds);
+
+}
 
 
 
